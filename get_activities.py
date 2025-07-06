733a63ef4c2c069bc0be609b702ce6806d82b086
joebowman21 <joebowman21@outlook.com>
2025-07-06 19:53:48 +0100

    Delete get_activities.py

--- get_activities.py
+++
@@ -1,8 +0,0 @@
-def get_activities(access_token, per_page=30):
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get(
-        "https://www.strava.com/api/v3/athlete/activities",
-        headers=headers,
-        params={'per_page': per_page}
-    )
-    return response.json()
