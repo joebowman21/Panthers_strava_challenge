e6e1517ad5efb250deb2c1fdb58f77f3e39ac4f6
Joe Bowman <joe.bowman@ba.com>
2025-10-23 12:59:58 +0100

    updating client id

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 # Your Strava app credentials
 # CLIENT_ID = os.getenv("CLIENT_ID")
 # CLIENT_SECRET = os.getenv("SECRET_ID")
-CLIENT_ID = '165743'
+CLIENT_ID = '165742'
 CLIENT_SECRET = '5ba3d1adb4e9face503194baee4cb9f0f16404e5'
 
 def refresh_access_token(refresh_token):
