ef59e04c42d7d167611519cfe38c0a1cfe72e3ac
Joe Bowman <joe.bowman@ba.com>
2025-10-23 12:47:16 +0100

    updating secrets

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from datetime import datetime, timedelta
 # Your Strava app credentials
 # CLIENT_ID = os.getenv("CLIENT_ID")
 # CLIENT_SECRET = os.getenv("SECRET_ID")
-CLIENT_ID = '165742'
-CLIENT_SECRET = 'd0015e5854fc1797ac8997d7bfb455f571ec3376'
+CLIENT_ID = '165743'
+CLIENT_SECRET = '5ba3d1adb4e9face503194baee4cb9f0f16404e5'
 
 def refresh_access_token(refresh_token):
     print(CLIENT_ID)
